340636241ce9a32c5a2d04f9cb6ed21ebc065c5b
DanielSinitski <d_sinitski@yahoo.de>
2026-05-05 17:05:32 +0200

    update gitignore

--- build/release/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/d_sin/VSCode Projects/market-simulator/build/release/_deps/googletest-src/googletest
-# Build directory: /mnt/c/Users/d_sin/VSCode Projects/market-simulator/build/release/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/sanitize/bench/CMakeFiles/hello_bench.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for hello_bench.
-# This may be replaced when dependencies are built.
